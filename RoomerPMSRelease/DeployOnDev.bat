e9707ea778cdbe5bb069aec3cfd51812e7a32e97
Bas Schouten <bas@roomerpms.com>
2016-11-28 13:25:37 +0100

    Added deployment batch files to dev.roomerstore (needs AWS config named "DEV")

---
+++ RoomerPMSRelease/DeployOnDev.bat
@@ -0,0 +1,37 @@
+@echo off
+setlocal
+
+set target=s3://store.roomerdev.net
+set awsprofile=--profile=dev
+set awsregion=--region=eu-west-1
+
+VersionXmlGenerator . > Roomer.xml
+
+if not exist "Deployment" mkdir Deployment
+
+copy Roomer.exe Deployment
+copy RoomerUpgradeAgent.exe Deployment
+copy Roomer.xml Deployment
+
+call SignSpecific Deployment\Roomer.exe
+call SignSpecific Deployment\RoomerUpgradeAgent.exe
+
+
+set AWSCMD="aws.exe"
+where /Q %AWSCMD%
+if errorlevel 1 (
+  Echo ERROR: AWS-CLI not found on path
+  goto :EOF
+)
+
+echo .
+set answer=
+set /p answer="Type yes to start copying to %target% ..."
+if "%answer%"=="yes" (
+  %AWSCMD% s3 %awsprofile% %awsregion% rm %target%/Roomer.xml
+  %AWSCMD% s3 %awsprofile% %awsregion% sync Deployment %target%
+  %AWSCMD% s3 %awsprofile% %awsregion% cp %target%/Roomer.exe %target%/roomer.exe
+ ) else (
+  echo Copying to %target% cancelled
+)
+endlocal
